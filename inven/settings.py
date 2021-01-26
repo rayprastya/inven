1a1683365965c11986e623c072e920d33688cfa0
rayprastya <54200114+rayprastya@users.noreply.github.com>
2021-01-26 15:48:33 +0700

    Update settings.py

--- inven/settings.py
+++ inven/settings.py
@@ -85,14 +85,13 @@ WSGI_APPLICATION = 'inven.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'inventorypy',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
     }
 }
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASE['default'].update(db_from_env)
 
 
 # Password validation
